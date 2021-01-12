1a1aeb8e3fbd4f917418ea4cd468388bd06bc591
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-01-12 21:43:03 +0300

    todo app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_hw/screens/todo_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
